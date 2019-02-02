b47bb007e5e534a336363b1b38546816360d081e
systena <systena@10.0.7.152>
2019-02-02 09:38:39 +0900

    2nd

---
+++ src/main/java/jp/co/systena/tigerscave/Monsters/MonstersApplication.java
@@ -0,0 +1,14 @@
+package jp.co.systena.tigerscave.Monsters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonstersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonstersApplication.class, args);
+	}
+
+}
+
